514fde10ef1b30ffdae3e4af42ed3443c6108906
foryichuanqi <48144488+foryichuanqi@users.noreply.github.com>
2022-09-14 19:43:06 +0800

    Add files via upload

---
+++ code/signal selection/min_loss_method/step1_min_loss.py
@@ -0,0 +1,312 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 20 22:06:23 2021
+
+@author: Administrator
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 18 09:53:38 2021
+
+@author: Administrator
+"""
+
+
+#import tensorflow as tf
+import os
+import numpy as np
+#from numpy import trans
+import matplotlib.pyplot as plt
+#import tensorflow as tf
+import CMAPSSDataset
+import pandas as pd
+import datetime
+import keras
+from keras.layers import Lambda
+import math
+import keras.backend as K
+import tensorflow as tf
+
+from sklearn.model_selection import train_test_split
+from keras.utils.vis_utils import plot_model
+
+
+
+def root_mean_squared_error(y_true, y_pred):
+        return K.sqrt(K.mean(K.square(y_pred - y_true),axis=0))##################  axis=0
+
+def rmse(predictions, targets):
+    return np.sqrt(((predictions - targets) ** 2).mean())
+
+
+
+
+    
+
+
+num_test=100
+
+run_times=10
+
+VALIDATION_SPLIT=0.3
+
+nb_epochs=2000            
+batch_size=1024   
+
+
+patience=50
+patience_reduce_lr=20
+
+
+
+
+num_filter1=64
+num_filter2=128
+num_filter3=64
+
+
+
+kernel1_size=16
+kernel2_size=10
+kernel3_size=6
+
+
+
+
+####31,21,38,19
+for FD in['1','2','3','4']: ######['1','2','3','4']
+
+    
+    FD_feature_columns=[]
+    # if FD=='1':
+    #     sequence_length=31
+    #     FD_feature_columns=['setting1', 'setting2', 's2', 's3','s4', 's7', 's8','s9', 's11', 's12', 's13', 's14', 's15', 's17', 's20', 's21']
+    
+    # if FD=='2':
+    #     sequence_length=21
+    #     FD_feature_columns=['s3','s4','s11','s15','s17']
+    # if FD=='3':
+    #     sequence_length=38
+    #     FD_feature_columns=['setting1', 'setting2',  's2', 's3','s4',  's7', 's8','s9', 's11', 's12', 's13', 's14', 's15', 's17', 's20', 's21']
+    # if FD=='4':
+    #     sequence_length=19    
+    #     FD_feature_columns=['s3','s4','s11','s15','s17']#['s3','s4','s11','s17']
+    
+    
+    
+    # if FD=='1':
+    #     sequence_length=31
+    #     FD_feature_columns=['setting1', 'setting2', 's2', 's3','s4', 's7', 's8','s9', 's11', 's12', 's13', 's14', 's15', 's17', 's20', 's21']
+    
+    # if FD=='2':
+    #     sequence_length=21
+    #     FD_feature_columns=['setting1', 'setting2', 's2', 's3','s4', 's7', 's8','s9', 's11', 's12', 's13', 's14', 's15', 's17', 's20', 's21']
+    # if FD=='3':
+    #     sequence_length=38
+    #     FD_feature_columns=['setting1', 'setting2', 's2', 's3','s4', 's7', 's8','s9', 's11', 's12', 's13', 's14', 's15', 's17', 's20', 's21']
+    # if FD=='4':
+    #     sequence_length=19    
+    #     FD_feature_columns=['setting1', 'setting2', 's2', 's3','s4', 's7', 's8','s9', 's11', 's12', 's13', 's14', 's15', 's17', 's20', 's21']
+
+    
+
+
+#####################################333333333333333333333333333333333
+    
+    # if FD=='1':
+    #     sequence_length=31
+    #     FD_feature_columns=['setting1', 'setting2', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's11', 's12', 's13', 's14', 's15', 's16', 's17', 's20', 's21']
+
+    # if FD=='2':
+    #     sequence_length=21
+    #     FD_feature_columns=['setting1', 'setting2', 'setting3', 's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20', 's21']
+    # if FD=='3':
+    #     sequence_length=38
+    #     FD_feature_columns=[ 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14', 's15', 's16', 's17', 's20', 's21','setting1', 'setting2']
+    # if FD=='4':
+    #     sequence_length=19    
+    #     FD_feature_columns=['setting1', 'setting2', 'setting3', 's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20', 's21']
+
+
+
+
+#################################    44444444444444444444444444
+    
+    # if FD=='1':
+    #     sequence_length=31
+    #     FD_feature_columns=['setting1', 'setting2', 'setting3', 's1', 's2', 's3','s4', 's5', 's6', 's7', 's8','s9', 's10', 's11', 's12', 's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20', 's21']
+
+    # if FD=='2':
+    #     sequence_length=21
+    #     FD_feature_columns=['setting1', 'setting2', 'setting3', 's1', 's2', 's3','s4', 's5', 's6', 's7', 's8','s9', 's10', 's11', 's12', 's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20', 's21']
+    # if FD=='3':
+    #     sequence_length=38
+    #     FD_feature_columns=['setting1', 'setting2', 'setting3', 's1', 's2', 's3','s4', 's5', 's6', 's7', 's8','s9', 's10', 's11', 's12', 's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20', 's21']
+    # if FD=='4':
+    #     sequence_length=19    
+    #     FD_feature_columns=['setting1', 'setting2', 'setting3', 's1', 's2', 's3','s4', 's5', 's6', 's7', 's8','s9', 's10', 's11', 's12', 's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20', 's21']
+
+
+ #############################5555555555555555555555555555   
+
+    
+    # if FD=='1':
+    #     sequence_length=31
+    #     FD_feature_columns=[  's2', 's3','s4',  's7', 's8','s9', 's11', 's12', 's13', 's14', 's15', 's17',  's20', 's21']
+
+    # if FD=='2':
+    #     sequence_length=21
+    #     FD_feature_columns=[  's2', 's3','s4',  's7', 's8','s9', 's11', 's12', 's13', 's14', 's15', 's17',  's20', 's21']
+    # if FD=='3':
+    #     sequence_length=38
+    #     FD_feature_columns=[  's2', 's3','s4',  's7', 's8','s9', 's11', 's12', 's13', 's14', 's15', 's17',  's20', 's21']
+    # if FD=='4':
+    #     sequence_length=19    
+    #     FD_feature_columns=[  's2', 's3','s4',  's7', 's8','s9', 's11', 's12', 's13', 's14', 's15', 's17',  's20', 's21']
+        
+
+
+#############################666666666666666666666666666666666            
+    # if FD=='1':
+    #     sequence_length=31
+    #     FD_feature_columns=[  's2', 's3','s4',  's7', 's8','s9', 's11', 's12', 's13', 's14', 's15', 's17',  's20', 's21']
+
+    # if FD=='2':
+    #     sequence_length=21
+    #     FD_feature_columns=['s1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20', 's21']
+    # if FD=='3':
+    #     sequence_length=38
+    #     FD_feature_columns=[  's2', 's3','s4',  's7', 's8','s9', 's11', 's12', 's13', 's14', 's15', 's17',  's20', 's21']
+    # if FD=='4':
+    #     sequence_length=19    
+    #     FD_feature_columns=['s1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20', 's21']
+
+
+#############777777777777777777777777777
+
+
+    if FD=='1':
+        sequence_length=31
+        FD_feature_columns=[ 's1', 's2', 's3','s4', 's5', 's6', 's7', 's8','s9', 's10', 's11', 's12', 's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20', 's21']
+
+    if FD=='2':
+        sequence_length=21
+        FD_feature_columns=[ 's1', 's2', 's3','s4', 's5', 's6', 's7', 's8','s9', 's10', 's11', 's12', 's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20', 's21']
+    if FD=='3':
+        sequence_length=38
+        FD_feature_columns=['s1', 's2', 's3','s4', 's5', 's6', 's7', 's8','s9', 's10', 's11', 's12', 's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20', 's21']
+    if FD=='4':
+        sequence_length=19    
+        FD_feature_columns=['s1', 's2', 's3','s4', 's5', 's6', 's7', 's8','s9', 's10', 's11', 's12', 's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20', 's21']
+
+
+
+
+
+
+    method_name='grid_FD{}_multi_channel_one_exp_attetion_num_test{}'.format(FD,num_test)
+    # method_name='FCN_RUL_1out_train_split_test'
+    dataset='cmapssd'
+    
+    
+    def unbalanced_penalty_score_1out(Y_test,Y_pred) :
+          
+        s=0    
+        for i in range(len(Y_pred)):
+            if Y_pred[i]>Y_test[i]:
+                s=s+math.exp((Y_pred[i]-Y_test[i])/10)-1
+            else:
+                s=s+math.exp((Y_test[i]-Y_pred[i])/13)-1    
+        print('unbalanced_penalty_score{}'.format(s))
+        return s  
+      
+    def error_range_1out(Y_test,Y_pred) :           
+        error_range=(Y_test-Y_pred).min(),(Y_test-Y_pred).max()
+        print('error range{}'.format(error_range))
+        return error_range
+    
+
+    
+    
+    # all_feature_columns =['setting1', 'setting2', 'setting3', 's1', 's2', 's3','s4', 's5', 's6', 's7', 's8','s9', 's10', 's11', 's12', 's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20', 's21']
+    # for i_all_feature_columns in range(len(all_feature_columns)):
+        
+    datasets = CMAPSSDataset.CMAPSSDataset(fd_number=FD, batch_size=batch_size, sequence_length=sequence_length,deleted_engine=[1000],feature_columns = FD_feature_columns)#deleted_engine=[5,17,31,41,46,55,69,73,82,95]
+    
+    
+    train_data = datasets.get_train_data()
+    train_feature_slice = datasets.get_feature_slice(train_data)
+    train_label_slice = datasets.get_label_slice(train_data)
+    
+    # valid_feature_slice = datasets.get_valid_feature_slice(train_data)
+    # valid_label_slice = datasets.get_valid_label_slice(train_data)
+    
+    
+    
+    print("train_data.shape: {}".format(train_data.shape))
+    print("train_feature_slice.shape: {}".format(train_feature_slice.shape))
+    print("train_label_slice.shape: {}".format(train_label_slice.shape))
+    
+    
+    test_data = datasets.get_test_data()
+    if num_test==100:
+        
+        test_feature_slice, test_label_slice = datasets.get_last_data_slice(test_data)
+        
+    if num_test==10000:
+        
+        test_feature_slice = datasets.get_feature_slice(test_data)
+        test_label_slice = datasets.get_label_slice(test_data)
+    # test_feature_slice, test_label_slice = datasets.get_last_data_slice(test_data)
+    
+    
+    print("test_data.shape: {}".format(test_data.shape))
+    print("test_feature_slice.shape: {}".format(test_feature_slice.shape))
+    print("test_label_slice.shape: {}".format(test_label_slice.shape))
+    
+    timesteps = train_feature_slice.shape[1]
+    input_dim = train_feature_slice.shape[2]
+    
+    #train_feature_slice=np.transpose( train_feature_slice,(0,2,1))
+    #
+    #
+    #test_feature_slice=np.transpose( test_feature_slice,(0,2,1))
+    
+    
+    
+    X_train=np.reshape(train_feature_slice,(-1,train_feature_slice.shape[1],1,train_feature_slice.shape[2]))
+    train_label_slice[train_label_slice>115]=115
+    Y_train=train_label_slice
+    
+    N=Y_train.shape[0]
+    
+    
+    print('FD{}_N={}'.format(FD,N))
+    
+    A=0
+    for i in range(Y_train.shape[0]):
+        A+=Y_train[i][0]
+    
+    A=-2*A/N  
+    
+    
+    print('FD{}A={}'.format(FD,A))
+    B=0
+    for i in range(Y_train.shape[0]):
+        B+=pow(Y_train[i][0],2)/N
+        
+    print('FD{}B={}'.format(FD,B))
+    
+    minloss=((-0.5*A)**2-A**2/2+B)**0.5    ########  loosb in Eq.(1),where x=-0.5*A
+    print('FD{}minloss={}'.format(FD,minloss))
+    
+
+            
+        
+            
+        
+
+
+
+
---
+++ code/signal selection/min_loss_method/step2_FD_get_min_last_loss_in_3_epoch_and_sum_loss.py
@@ -0,0 +1,335 @@
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  7 20:11:11 2021
+
+@author: Administrator
+"""
+
+import os 
+
+print(os.path.abspath(os.path.join(os.getcwd(), "../../..")))
+last_last_last_path=os.path.abspath(os.path.join(os.getcwd(), "../../.."))
+import  pandas  as pd 
+import numpy as np
+def Get_Average(list):
+
+    sum = 0
+    
+    for item in list:
+    
+        sum += item
+    
+    return sum/len(list)  
+
+
+def get_mp_and_selected_feature(list_last_loss_3epoches,fd_min,shredshold):
+    mp=[]
+    for i in range(len(list_last_loss_3epoches)):
+        mp.append((fd_min-list_last_loss_3epoches[i])/fd_min)
+    feature_list=['setting1', 'setting2', 'setting3', 's1_', 's2_', 's3','s4', 's5', 's6', 's7', 's8',
+        's9', 's10', 's11', 's12', 's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20', 's21']
+    selected_feature=[]
+    for i in range(len(list_last_loss_3epoches)):
+        if mp[i]>shredshold:
+            selected_feature.append(feature_list[i])
+    
+    return mp,selected_feature
+
+
+shredshold=0          ######### mp_min in Fig.1 of the paper 
+
+def listdir(path, list_name):  #传入存储的list
+    for file in os.listdir(path):  
+        file_path = os.path.join(path, file)  
+        if os.path.isdir(file_path):  
+            listdir(file_path, list_name)  
+        else:  
+            list_name.append(file_path)
+list_name=[]
+listdir(last_last_last_path + r'\experiments_result\log\fd\fd1',list_name)
+feature_columns = ['setting1', 'setting2', 'setting3', 's1_', 's2_', 's3','s4', 's5', 's6', 's7', 's8',
+        's9', 's10', 's11', 's12', 's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20', 's21']
+list_same_feature=[]
+list_last_loss_3epoches=[]
+list_first_divide_last_loss_3epoches=[]
+list_sum_loss_3epoches=[]
+for sub_feature in feature_columns:
+    for sub_list in list_name:
+        if sub_feature in sub_list:
+            list_same_feature.append(sub_list)
+        list_sum_loss=[]
+        list_last_loss=[]
+        list_first_loss=[]
+        for same_feature in list_same_feature:
+            
+            df = pd.read_excel(same_feature)  #读取xlsx中第一个sheet
+
+            loss=list(df['root_mean_squared_error']) 
+            list_last_loss.append(Get_Average(loss[-10:]))
+            list_first_loss.append(loss[0])
+            list_sum_loss.append(np.sum(loss))
+       
+    list_last_loss_3epoches.append(min(list_last_loss))
+    # list_first_divide_last_loss_3epoches.append((list_first_loss[list_last_loss.index(min(list_last_loss))]-min(list_last_loss))/list_first_loss[list_last_loss.index(min(list_last_loss))])
+    # # print('last{}'.format(min(list_last_loss)))
+    # # print('first{}'.format(list_first_loss[list_last_loss.index(min(list_last_loss))]-min(list_last_loss)))
+    # # print('map_power{}'.format(list_first_divide_last_loss_3epoches[-1]))
+    # list_sum_loss_3epoches.append(list_sum_loss[list_last_loss.index(min(list_last_loss))])
+    
+    # list_sum_loss=[]
+    # list_last_loss=[] 
+    # list_first_loss=[]
+           
+            
+            
+    list_same_feature=[]
+
+print('FD1_min_average_last_10epoch_in_3experiment')
+
+print(list_last_loss_3epoches)
+# print(list_sum_loss_3epoches)
+# print(list_first_divide_last_loss_3epoches)
+
+fd1_min=38.50789128965064
+fd2_min=38.39994381297593
+fd3_min=37.90198469885309
+fd4_min=37.37423922740954
+
+mp,selected_feature=get_mp_and_selected_feature(list_last_loss_3epoches,fd1_min,shredshold)
+print('mp')
+
+print(mp)
+
+
+print('selected_feature')
+print(selected_feature)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print('\n')
+
+
+
+
+
+
+
+
+
+
+list_name=[]
+listdir(last_last_last_path + r'\experiments_result\log\fd\fd2',list_name)
+feature_columns = ['setting1', 'setting2', 'setting3', 's1_', 's2_', 's3','s4', 's5', 's6', 's7', 's8',
+        's9', 's10', 's11', 's12', 's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20', 's21']
+list_same_feature=[]
+list_last_loss_3epoches=[]
+list_first_divide_last_loss_3epoches=[]
+list_sum_loss_3epoches=[]
+for sub_feature in feature_columns:
+    for sub_list in list_name:
+        if sub_feature in sub_list:
+            list_same_feature.append(sub_list)
+        list_sum_loss=[]
+        list_last_loss=[]
+        list_first_loss=[]
+        for same_feature in list_same_feature:
+            
+            df = pd.read_excel(same_feature)  #读取xlsx中第一个sheet
+
+            loss=list(df['root_mean_squared_error']) 
+            list_last_loss.append(Get_Average(loss[-10:]))
+            list_first_loss.append(loss[0])
+            list_sum_loss.append(np.sum(loss))
+       
+    list_last_loss_3epoches.append(min(list_last_loss))
+    # list_first_divide_last_loss_3epoches.append((list_first_loss[list_last_loss.index(min(list_last_loss))]-min(list_last_loss))/list_first_loss[list_last_loss.index(min(list_last_loss))])
+    # # print('last{}'.format(min(list_last_loss)))
+    # # print('first{}'.format(list_first_loss[list_last_loss.index(min(list_last_loss))]-min(list_last_loss)))
+    # # print('map_power{}'.format(list_first_divide_last_loss_3epoches[-1]))
+    # list_sum_loss_3epoches.append(list_sum_loss[list_last_loss.index(min(list_last_loss))])
+    
+    # list_sum_loss=[]
+    # list_last_loss=[] 
+    # list_first_loss=[]
+           
+            
+            
+    list_same_feature=[]
+
+print('FD2_min_average_last_10epoch_in_3experiment')
+
+print(list_last_loss_3epoches)
+# print(list_sum_loss_3epoches)
+# print(list_first_divide_last_loss_3epoches)
+
+
+mp,selected_feature=get_mp_and_selected_feature(list_last_loss_3epoches,fd2_min,shredshold)
+print('mp')
+
+print(mp)
+
+
+print('selected_feature')
+print(selected_feature)
+
+
+
+
+
+
+
+print('\n')
+
+
+
+
+list_name=[]
+listdir(last_last_last_path + r'\experiments_result\log\fd\fd3',list_name)
+feature_columns = ['setting1', 'setting2', 'setting3', 's1_', 's2_', 's3','s4', 's5', 's6', 's7', 's8',
+        's9', 's10', 's11', 's12', 's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20', 's21']
+list_same_feature=[]
+list_last_loss_3epoches=[]
+list_first_divide_last_loss_3epoches=[]
+list_sum_loss_3epoches=[]
+for sub_feature in feature_columns:
+    for sub_list in list_name:
+        if sub_feature in sub_list:
+            list_same_feature.append(sub_list)
+        list_sum_loss=[]
+        list_last_loss=[]
+        list_first_loss=[]
+        for same_feature in list_same_feature:
+            
+            df = pd.read_excel(same_feature)  #读取xlsx中第一个sheet
+
+            loss=list(df['root_mean_squared_error']) 
+            list_last_loss.append(Get_Average(loss[-10:]))
+            list_first_loss.append(loss[0])
+            list_sum_loss.append(np.sum(loss))
+       
+    list_last_loss_3epoches.append(min(list_last_loss))
+    # list_first_divide_last_loss_3epoches.append((list_first_loss[list_last_loss.index(min(list_last_loss))]-min(list_last_loss))/list_first_loss[list_last_loss.index(min(list_last_loss))])
+    # # print('last{}'.format(min(list_last_loss)))
+    # # print('first{}'.format(list_first_loss[list_last_loss.index(min(list_last_loss))]-min(list_last_loss)))
+    # # print('map_power{}'.format(list_first_divide_last_loss_3epoches[-1]))
+    # list_sum_loss_3epoches.append(list_sum_loss[list_last_loss.index(min(list_last_loss))])
+    
+    # list_sum_loss=[]
+    # list_last_loss=[] 
+    # list_first_loss=[]
+           
+            
+            
+    list_same_feature=[]
+
+print('FD3_min_average_last_10epoch_in_3experiment')
+
+print(list_last_loss_3epoches)
+# print(list_sum_loss_3epoches)
+# print(list_first_divide_last_loss_3epoches)
+
+
+mp,selected_feature=get_mp_and_selected_feature(list_last_loss_3epoches,fd3_min,shredshold)
+print('mp')
+
+print(mp)
+
+
+print('selected_feature')
+print(selected_feature)
+
+
+
+
+
+
+
+print('\n')
+
+
+
+
+
+list_name=[]
+listdir(last_last_last_path + r'\experiments_result\log\fd\fd4',list_name)
+feature_columns = ['setting1', 'setting2', 'setting3', 's1_', 's2_', 's3','s4', 's5', 's6', 's7', 's8',
+        's9', 's10', 's11', 's12', 's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20', 's21']
+list_same_feature=[]
+list_last_loss_3epoches=[]
+list_first_divide_last_loss_3epoches=[]
+list_sum_loss_3epoches=[]
+for sub_feature in feature_columns:
+    for sub_list in list_name:
+        if sub_feature in sub_list:
+            list_same_feature.append(sub_list)
+        list_sum_loss=[]
+        list_last_loss=[]
+        list_first_loss=[]
+        for same_feature in list_same_feature:
+            
+            df = pd.read_excel(same_feature)  #读取xlsx中第一个sheet
+
+            loss=list(df['root_mean_squared_error']) 
+            list_last_loss.append(Get_Average(loss[-10:]))
+            list_first_loss.append(loss[0])
+            list_sum_loss.append(np.sum(loss))
+       
+    list_last_loss_3epoches.append(min(list_last_loss))
+    # list_first_divide_last_loss_3epoches.append((list_first_loss[list_last_loss.index(min(list_last_loss))]-min(list_last_loss))/list_first_loss[list_last_loss.index(min(list_last_loss))])
+    # # print('last{}'.format(min(list_last_loss)))
+    # # print('first{}'.format(list_first_loss[list_last_loss.index(min(list_last_loss))]-min(list_last_loss)))
+    # # print('map_power{}'.format(list_first_divide_last_loss_3epoches[-1]))
+    # list_sum_loss_3epoches.append(list_sum_loss[list_last_loss.index(min(list_last_loss))])
+    
+    # list_sum_loss=[]
+    # list_last_loss=[] 
+    # list_first_loss=[]
+           
+            
+            
+    list_same_feature=[]
+
+print('FD4_min_average_last_10epoch_in_3experiment')
+
+print(list_last_loss_3epoches)
+# print(list_sum_loss_3epoches)
+# print(list_first_divide_last_loss_3epoches)
+
+
+
+
+mp,selected_feature=get_mp_and_selected_feature(list_last_loss_3epoches,fd4_min,shredshold)
+print('mp')
+
+print(mp)
+
+
+print('selected_feature')
+print(selected_feature)
+
+
+
+
+
+print('\n')
+
+
+
+
+
+            
+            
+
